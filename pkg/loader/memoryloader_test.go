40c49f07d6c0a7a691eb2f289898655beb3ed04a
Jared Nishikawa <jared.nishikawa@gmail.com>
2025-11-16 23:22:21 -0700

    more tests

---
+++ pkg/loader/memoryloader_test.go
@@ -0,0 +1,72 @@
+/*
+Copyright (c) 2025 Jared Nishikawa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package loader_test
+
+import (
+	"testing"
+
+	"github.com/jarednogo/board/internal/assert"
+	"github.com/jarednogo/board/pkg/loader"
+)
+
+func TestMemoryLoader(t *testing.T) {
+	ml := loader.NewMemoryLoader()
+
+	assert.NoError(t, ml.Setup())
+
+	ml.AddMessage("test", 30)
+	assert.Equal(t, ml.MessageCount(), 1)
+
+	msgs, err := ml.LoadAllMessages()
+	assert.Equal(t, len(msgs), 1)
+	assert.NoError(t, err)
+
+	err = ml.DeleteAllMessages()
+	assert.NoError(t, err)
+
+	msgs, err = ml.LoadAllMessages()
+	assert.Equal(t, len(msgs), 0)
+	assert.NoError(t, err)
+
+	assert.Equal(t, ml.TwitchGetRoom("user123"), "")
+	err = ml.TwitchSetRoom("user123", "room1")
+	assert.NoError(t, err)
+	assert.Equal(t, ml.TwitchGetRoom("user123"), "room1")
+
+	// make a struct to save a room
+	l := &loader.LoadJSON{
+		SGF:       "test",
+		Location:  "test",
+		Buffer:    500,
+		NextIndex: 42,
+		Password:  "test",
+		ID:        "test",
+	}
+
+	_, err = ml.LoadRoom("room1")
+	assert.NotNil(t, err)
+	err = ml.SaveRoom("room1", l)
+	assert.NoError(t, err)
+	m, err := ml.LoadRoom("room1")
+	assert.Equal(t, m.SGF, l.SGF)
+	assert.NoError(t, err)
+
+	rooms, err := ml.LoadAllRooms()
+	assert.Equal(t, len(rooms), 1)
+	assert.NoError(t, err)
+
+	err = ml.DeleteRoom("room1")
+	assert.NoError(t, err)
+	rooms, err = ml.LoadAllRooms()
+	assert.Equal(t, len(rooms), 0)
+	assert.NoError(t, err)
+
+}
---
+++ pkg/state/command_decoder_test.go
@@ -0,0 +1,208 @@
+/*
+Copyright (c) 2025 Jared Nishikawa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package state_test
+
+import (
+	"testing"
+
+	"github.com/jarednogo/board/internal/assert"
+	"github.com/jarednogo/board/pkg/event"
+	"github.com/jarednogo/board/pkg/state"
+)
+
+func TestDecodeAddStone(t *testing.T) {
+	val := make(map[string]any)
+	val["color"] = 1.0
+	val["coords"] = []any{9.0, 9.0}
+	e := event.NewEvent("add_stone", val)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodePass(t *testing.T) {
+	e := event.NewEvent("pass", 1.0)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeRemoveStone(t *testing.T) {
+	val := []any{9.0, 9.0}
+	e := event.NewEvent("remove_stone", val)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeTriangle(t *testing.T) {
+	val := []any{9.0, 9.0}
+	e := event.NewEvent("triangle", val)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeSquare(t *testing.T) {
+	val := []any{9.0, 9.0}
+	e := event.NewEvent("square", val)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeLetter(t *testing.T) {
+	val := make(map[string]any)
+	val["coords"] = []any{9.0, 9.0}
+	val["letter"] = "A"
+	e := event.NewEvent("letter", val)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeNumber(t *testing.T) {
+	val := make(map[string]any)
+	val["coords"] = []any{9.0, 9.0}
+	val["number"] = 1.0
+	e := event.NewEvent("number", val)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeCut(t *testing.T) {
+	e := event.NewEvent("cut", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeLeft(t *testing.T) {
+	e := event.NewEvent("left", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeRight(t *testing.T) {
+	e := event.NewEvent("right", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeUp(t *testing.T) {
+	e := event.NewEvent("up", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeDown(t *testing.T) {
+	e := event.NewEvent("down", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeRewind(t *testing.T) {
+	e := event.NewEvent("rewind", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeFastForward(t *testing.T) {
+	e := event.NewEvent("fastforward", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeGotoGrid(t *testing.T) {
+	e := event.NewEvent("goto_grid", 3.0)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeGotoCoord(t *testing.T) {
+	val := []any{9.0, 9.0}
+	e := event.NewEvent("goto_coord", val)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeComment(t *testing.T) {
+	e := event.NewEvent("comment", "somecomment")
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeDraw(t *testing.T) {
+	val := []any{0.0, 0.0, 1.0, 1.0, "#445566"}
+	e := event.NewEvent("draw", val)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeErasePen(t *testing.T) {
+	e := event.NewEvent("erase_pen", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeCopy(t *testing.T) {
+	e := event.NewEvent("copy", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeClipboard(t *testing.T) {
+	e := event.NewEvent("clipboard", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeGraft(t *testing.T) {
+	e := event.NewEvent("graft", "a1 a2 a3 a4")
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeScore(t *testing.T) {
+	e := event.NewEvent("score", nil)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeMarkDead(t *testing.T) {
+	val := []any{9.0, 9.0}
+	e := event.NewEvent("markdead", val)
+
+	_, err := state.DecodeToCommand(e)
+	assert.NoError(t, err)
+}
+
+func TestDecodeFail(t *testing.T) {
+	e := event.NewEvent("failevent", nil)
+	_, err := state.DecodeToCommand(e)
+	assert.NotNil(t, err)
+}
