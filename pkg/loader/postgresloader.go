9337078dee1ea06d309d89c2d59b2ab8f2245112
Jared Nishikawa <jared.nishikawa@gmail.com>
2025-11-25 17:04:14 -0700

    splitting postgresloader from sqliteloader

--- pkg/loader/postgresloader.go
+++ pkg/loader/postgresloader.go
@@ -11,14 +11,20 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 package loader
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"time"
 
 	// blank import necessary for postgres driver
 	_ "github.com/lib/pq"
 )
 
-func NewPostgresLoader(dsn string) (*DBLoader, error) {
+type PostgresLoader struct {
+	db *sql.DB
+}
+
+func NewPostgresLoader(dsn string) (*PostgresLoader, error) {
 	var err error
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
@@ -39,7 +45,7 @@ func NewPostgresLoader(dsn string) (*DBLoader, error) {
 		return nil, err
 	}
 
-	ldr := &DBLoader{db}
+	ldr := &PostgresLoader{db}
 	err = ldr.setup()
 
 	if err != nil {
@@ -47,4 +53,286 @@ func NewPostgresLoader(dsn string) (*DBLoader, error) {
 	}
 
 	return ldr, nil
+}
+
+func (ldr *PostgresLoader) setup() error {
+	// Create the 'rooms' table if it doesn't exist
+	_, err := ldr.db.ExecContext(
+		context.Background(),
+		`CREATE TABLE IF NOT EXISTS rooms (
+			id TEXT PRIMARY KEY,
+			sgf TEXT NOT NULL,
+			loc TEXT NOT NULL,
+			prefs TEXT NOT NULL,
+			buffer INTEGER NOT NULL,
+			nextindex INTEGER NOT NULL,
+			password TEXT NOT NULL
+		)`,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	// Create the 'messages' table if it doesn't exist
+	_, err = ldr.db.ExecContext(
+		context.Background(),
+		`CREATE TABLE IF NOT EXISTS messages (
+			id SERIAL PRIMARY KEY,
+			text TEXT NOT NULL,
+			ttl INTEGER NOT NULL
+		)`,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	// create the 'twitch' table if it doesn't exist
+	_, err = ldr.db.ExecContext(
+		context.Background(),
+		`CREATE TABLE IF NOT EXISTS twitch (
+			id SERIAL PRIMARY KEY,
+			broadcaster TEXT NOT NULL,
+			roomid TEXT
+		)`,
+	)
+
+	return err
+}
+
+func (ldr *PostgresLoader) Close() error {
+	return ldr.db.Close()
+}
+
+// Twitch logic
+
+func (ldr *PostgresLoader) TwitchSetRoom(broadcaster, roomid string) error {
+	rooms, err := ldr.twitchSelectRoom(broadcaster)
+	if err != nil {
+		return err
+	}
+
+	if len(rooms) == 0 {
+		return ldr.twitchInsertRoom(broadcaster, roomid)
+	} else {
+		return ldr.twitchUpdateRoom(broadcaster, roomid)
+	}
+}
+
+func (ldr *PostgresLoader) TwitchGetRoom(broadcaster string) string {
+	rooms, err := ldr.twitchSelectRoom(broadcaster)
+	if err != nil || len(rooms) != 1 {
+		return ""
+	}
+	return rooms[0]
+}
+
+func (ldr *PostgresLoader) twitchSelectRoom(broadcaster string) ([]string, error) {
+	rows, err := ldr.db.QueryContext(
+		context.Background(),
+		`SELECT roomid FROM twitch WHERE broadcaster = $1`,
+		broadcaster)
+	if err != nil {
+		return []string{}, err
+	}
+
+	defer rows.Close() //nolint: errcheck
+	rooms := []string{}
+	for rows.Next() {
+		var roomid string
+		err = rows.Scan(&roomid)
+		if err != nil {
+			continue
+		}
+		rooms = append(rooms, roomid)
+	}
+
+	return rooms, nil
+}
+
+func (ldr *PostgresLoader) twitchInsertRoom(broadcaster, roomid string) error {
+	_, err := ldr.db.ExecContext(
+		context.Background(),
+		`INSERT INTO twitch (broadcaster, roomid) VALUES ($1, $2)`,
+		broadcaster, roomid)
+	return err
+}
+
+func (ldr *PostgresLoader) twitchUpdateRoom(broadcaster, roomid string) error {
+	_, err := ldr.db.ExecContext(
+		context.Background(),
+		`UPDATE twitch SET roomid = $1 WHERE broadcaster = $2`,
+		roomid, broadcaster)
+	return err
+}
+
+func (ldr *PostgresLoader) LoadRoom(id string) (*LoadJSON, error) {
+	rows, err := ldr.selectRoom(id)
+	if err != nil {
+		return nil, err
+	}
+	if len(rows) != 1 {
+		return nil, fmt.Errorf("incorrect number of rows returned: %d", len(rows))
+	}
+	return rows[0], nil
+}
+
+// Save could also reasonably be called InsertOrUpdate
+func (ldr *PostgresLoader) SaveRoom(id string, data *LoadJSON) error {
+	rows, err := ldr.selectRoom(id)
+	if err != nil {
+		return err
+	}
+	if len(rows) != 0 {
+		return ldr.updateRoom(id, data)
+	} else {
+		return ldr.insertRoom(id, data)
+	}
+}
+
+func (ldr *PostgresLoader) DeleteRoom(id string) error {
+	_, err := ldr.db.Exec(`DELETE FROM rooms WHERE id = $1`, id)
+	return err
+}
+
+func (ldr *PostgresLoader) updateRoom(id string, data *LoadJSON) error {
+	prefs, err := Prefs(data.Prefs).ToString()
+	if err != nil {
+		return err
+	}
+
+	_, err = ldr.db.ExecContext(
+		context.Background(),
+		`UPDATE rooms SET sgf = $1, loc = $2, prefs = $3, buffer = $4, nextindex = $5, password = $6  WHERE id = $7`,
+		data.SGF, data.Location, prefs, data.Buffer, data.NextIndex, data.Password, id)
+	return err
+}
+
+func (ldr *PostgresLoader) insertRoom(id string, data *LoadJSON) error {
+	prefs, err := Prefs(data.Prefs).ToString()
+	if err != nil {
+		return err
+	}
+
+	_, err = ldr.db.ExecContext(
+		context.Background(),
+		`INSERT INTO rooms (id, sgf, loc, prefs, buffer, nextindex, password) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+		id, data.SGF, data.Location, prefs, data.Buffer, data.NextIndex, data.Password)
+
+	return err
+}
+
+func (ldr *PostgresLoader) LoadAllRooms() ([]*LoadJSON, error) {
+	rows, err := ldr.db.QueryContext(
+		context.Background(),
+		`SELECT id, sgf, loc, prefs, buffer, nextindex, password FROM rooms`)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close() //nolint: errcheck
+	var rooms []*LoadJSON
+	for rows.Next() {
+		var id string
+		var sgf string
+		var loc string
+		var prefsString string
+		var buffer int
+		var nextIndex int
+		var password string
+		err = rows.Scan(&id, &sgf, &loc, &prefsString, &buffer, &nextIndex, &password)
+		if err != nil {
+			return nil, err
+		}
+		prefs, _ := PrefsFromString(prefsString)
+		data := &LoadJSON{
+			SGF:       sgf,
+			Location:  loc,
+			Prefs:     prefs,
+			Buffer:    int64(buffer),
+			NextIndex: nextIndex,
+			Password:  password,
+			ID:        id,
+		}
+		rooms = append(rooms, data)
+	}
+
+	return rooms, nil
+}
+
+func (ldr *PostgresLoader) selectRoom(id string) ([]*LoadJSON, error) {
+	rows, err := ldr.db.QueryContext(
+		context.Background(),
+		`SELECT id, sgf, loc, prefs, buffer, nextindex, password FROM rooms WHERE id = $1`,
+		id)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close() //nolint: errcheck
+	var rooms []*LoadJSON
+	for rows.Next() {
+		var id string
+		var sgf string
+		var loc string
+		var prefsString string
+		var buffer int
+		var nextIndex int
+		var password string
+		err = rows.Scan(&id, &sgf, &loc, &prefsString, &buffer, &nextIndex, &password)
+		if err != nil {
+			return nil, err
+		}
+		prefs, _ := PrefsFromString(prefsString)
+		data := &LoadJSON{
+			SGF:       sgf,
+			Location:  loc,
+			Prefs:     prefs,
+			Buffer:    int64(buffer),
+			NextIndex: nextIndex,
+			Password:  password,
+			ID:        id,
+		}
+		rooms = append(rooms, data)
+	}
+
+	return rooms, nil
+}
+
+func (ldr *PostgresLoader) LoadAllMessages() ([]*MessageJSON, error) {
+	rows, err := ldr.db.QueryContext(
+		context.Background(),
+		`SELECT text, ttl FROM messages`)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close() //nolint: errcheck
+	var messages []*MessageJSON
+	for rows.Next() {
+		var text string
+		var ttl int
+		err = rows.Scan(&text, &ttl)
+		if err != nil {
+			return nil, err
+		}
+		msg := &MessageJSON{
+			Text: text,
+			TTL:  ttl,
+		}
+		messages = append(messages, msg)
+	}
+
+	return messages, nil
+}
+
+func (ldr *PostgresLoader) DeleteAllMessages() error {
+	_, err := ldr.db.Exec(`DELETE FROM messages`)
+	return err
+}
+
+func (ldr *PostgresLoader) InsertTestMessage() error {
+	_, err := ldr.db.Exec(`INSERT INTO messages (text, ttl) VALUES ("foo", 60)`)
+	return err
 }
--- pkg/loader/sqliteloader.go
+++ pkg/loader/sqliteloader.go
@@ -11,7 +11,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 package loader
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -21,7 +23,11 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-func NewSqliteLoader(path string) (*DBLoader, error) {
+type SqliteLoader struct {
+	db *sql.DB
+}
+
+func NewSqliteLoader(path string) (*SqliteLoader, error) {
 	if err := mkDirs(path); err != nil {
 		return nil, err
 	}
@@ -29,7 +35,7 @@ func NewSqliteLoader(path string) (*DBLoader, error) {
 	if err != nil {
 		return nil, err
 	}
-	ldr := &DBLoader{db}
+	ldr := &SqliteLoader{db}
 	err = ldr.setup()
 
 	if err != nil {
@@ -47,4 +53,286 @@ func mkDirs(path string) error {
 		return err
 	}
 	return nil
+}
+
+func (ldr *SqliteLoader) setup() error {
+	// Create the 'rooms' table if it doesn't exist
+	_, err := ldr.db.ExecContext(
+		context.Background(),
+		`CREATE TABLE IF NOT EXISTS rooms (
+			id TEXT PRIMARY KEY,
+			sgf TEXT NOT NULL,
+			loc TEXT NOT NULL,
+			prefs TEXT NOT NULL,
+			buffer INTEGER NOT NULL,
+			nextindex INTEGER NOT NULL,
+			password TEXT NOT NULL
+		)`,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	// Create the 'messages' table if it doesn't exist
+	_, err = ldr.db.ExecContext(
+		context.Background(),
+		`CREATE TABLE IF NOT EXISTS messages (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			text TEXT NOT NULL,
+			ttl INTEGER NOT NULL
+		)`,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	// create the 'twitch' table if it doesn't exist
+	_, err = ldr.db.ExecContext(
+		context.Background(),
+		`CREATE TABLE IF NOT EXISTS twitch (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			broadcaster TEXT NOT NULL,
+			roomid TEXT
+		)`,
+	)
+
+	return err
+}
+
+func (ldr *SqliteLoader) Close() error {
+	return ldr.db.Close()
+}
+
+// Twitch logic
+
+func (ldr *SqliteLoader) TwitchSetRoom(broadcaster, roomid string) error {
+	rooms, err := ldr.twitchSelectRoom(broadcaster)
+	if err != nil {
+		return err
+	}
+
+	if len(rooms) == 0 {
+		return ldr.twitchInsertRoom(broadcaster, roomid)
+	} else {
+		return ldr.twitchUpdateRoom(broadcaster, roomid)
+	}
+}
+
+func (ldr *SqliteLoader) TwitchGetRoom(broadcaster string) string {
+	rooms, err := ldr.twitchSelectRoom(broadcaster)
+	if err != nil || len(rooms) != 1 {
+		return ""
+	}
+	return rooms[0]
+}
+
+func (ldr *SqliteLoader) twitchSelectRoom(broadcaster string) ([]string, error) {
+	rows, err := ldr.db.QueryContext(
+		context.Background(),
+		`SELECT roomid FROM twitch WHERE broadcaster = ?`,
+		broadcaster)
+	if err != nil {
+		return []string{}, err
+	}
+
+	defer rows.Close() //nolint: errcheck
+	rooms := []string{}
+	for rows.Next() {
+		var roomid string
+		err = rows.Scan(&roomid)
+		if err != nil {
+			continue
+		}
+		rooms = append(rooms, roomid)
+	}
+
+	return rooms, nil
+}
+
+func (ldr *SqliteLoader) twitchInsertRoom(broadcaster, roomid string) error {
+	_, err := ldr.db.ExecContext(
+		context.Background(),
+		`INSERT INTO twitch (broadcaster, roomid) VALUES (?, ?)`,
+		broadcaster, roomid)
+	return err
+}
+
+func (ldr *SqliteLoader) twitchUpdateRoom(broadcaster, roomid string) error {
+	_, err := ldr.db.ExecContext(
+		context.Background(),
+		`UPDATE twitch SET roomid = ? WHERE broadcaster = ?`,
+		roomid, broadcaster)
+	return err
+}
+
+func (ldr *SqliteLoader) LoadRoom(id string) (*LoadJSON, error) {
+	rows, err := ldr.selectRoom(id)
+	if err != nil {
+		return nil, err
+	}
+	if len(rows) != 1 {
+		return nil, fmt.Errorf("incorrect number of rows returned: %d", len(rows))
+	}
+	return rows[0], nil
+}
+
+// Save could also reasonably be called InsertOrUpdate
+func (ldr *SqliteLoader) SaveRoom(id string, data *LoadJSON) error {
+	rows, err := ldr.selectRoom(id)
+	if err != nil {
+		return err
+	}
+	if len(rows) != 0 {
+		return ldr.updateRoom(id, data)
+	} else {
+		return ldr.insertRoom(id, data)
+	}
+}
+
+func (ldr *SqliteLoader) DeleteRoom(id string) error {
+	_, err := ldr.db.Exec(`DELETE FROM rooms WHERE id = ?`, id)
+	return err
+}
+
+func (ldr *SqliteLoader) updateRoom(id string, data *LoadJSON) error {
+	prefs, err := Prefs(data.Prefs).ToString()
+	if err != nil {
+		return err
+	}
+
+	_, err = ldr.db.ExecContext(
+		context.Background(),
+		`UPDATE rooms SET sgf = ?, loc = ?, prefs = ?, buffer = ?, nextindex = ?, password = ?  WHERE id = ?`,
+		data.SGF, data.Location, prefs, data.Buffer, data.NextIndex, data.Password, id)
+	return err
+}
+
+func (ldr *SqliteLoader) insertRoom(id string, data *LoadJSON) error {
+	prefs, err := Prefs(data.Prefs).ToString()
+	if err != nil {
+		return err
+	}
+
+	_, err = ldr.db.ExecContext(
+		context.Background(),
+		`INSERT INTO rooms (id, sgf, loc, prefs, buffer, nextindex, password) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+		id, data.SGF, data.Location, prefs, data.Buffer, data.NextIndex, data.Password)
+
+	return err
+}
+
+func (ldr *SqliteLoader) LoadAllRooms() ([]*LoadJSON, error) {
+	rows, err := ldr.db.QueryContext(
+		context.Background(),
+		`SELECT id, sgf, loc, prefs, buffer, nextindex, password FROM rooms`)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close() //nolint: errcheck
+	var rooms []*LoadJSON
+	for rows.Next() {
+		var id string
+		var sgf string
+		var loc string
+		var prefsString string
+		var buffer int
+		var nextIndex int
+		var password string
+		err = rows.Scan(&id, &sgf, &loc, &prefsString, &buffer, &nextIndex, &password)
+		if err != nil {
+			return nil, err
+		}
+		prefs, _ := PrefsFromString(prefsString)
+		data := &LoadJSON{
+			SGF:       sgf,
+			Location:  loc,
+			Prefs:     prefs,
+			Buffer:    int64(buffer),
+			NextIndex: nextIndex,
+			Password:  password,
+			ID:        id,
+		}
+		rooms = append(rooms, data)
+	}
+
+	return rooms, nil
+}
+
+func (ldr *SqliteLoader) selectRoom(id string) ([]*LoadJSON, error) {
+	rows, err := ldr.db.QueryContext(
+		context.Background(),
+		`SELECT id, sgf, loc, prefs, buffer, nextindex, password FROM rooms WHERE id = ?`,
+		id)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close() //nolint: errcheck
+	var rooms []*LoadJSON
+	for rows.Next() {
+		var id string
+		var sgf string
+		var loc string
+		var prefsString string
+		var buffer int
+		var nextIndex int
+		var password string
+		err = rows.Scan(&id, &sgf, &loc, &prefsString, &buffer, &nextIndex, &password)
+		if err != nil {
+			return nil, err
+		}
+		prefs, _ := PrefsFromString(prefsString)
+		data := &LoadJSON{
+			SGF:       sgf,
+			Location:  loc,
+			Prefs:     prefs,
+			Buffer:    int64(buffer),
+			NextIndex: nextIndex,
+			Password:  password,
+			ID:        id,
+		}
+		rooms = append(rooms, data)
+	}
+
+	return rooms, nil
+}
+
+func (ldr *SqliteLoader) LoadAllMessages() ([]*MessageJSON, error) {
+	rows, err := ldr.db.QueryContext(
+		context.Background(),
+		`SELECT text, ttl FROM messages`)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close() //nolint: errcheck
+	var messages []*MessageJSON
+	for rows.Next() {
+		var text string
+		var ttl int
+		err = rows.Scan(&text, &ttl)
+		if err != nil {
+			return nil, err
+		}
+		msg := &MessageJSON{
+			Text: text,
+			TTL:  ttl,
+		}
+		messages = append(messages, msg)
+	}
+
+	return messages, nil
+}
+
+func (ldr *SqliteLoader) DeleteAllMessages() error {
+	_, err := ldr.db.Exec(`DELETE FROM messages`)
+	return err
+}
+
+func (ldr *SqliteLoader) InsertTestMessage() error {
+	_, err := ldr.db.Exec(`INSERT INTO messages (text, ttl) VALUES ("foo", 60)`)
+	return err
 }
